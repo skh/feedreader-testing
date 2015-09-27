f326d73655177e97cee30e5473301ffeba33962a
Sonja Krause-Harder <skh@rcane.de>
2015-09-27 15:58:15 +0200

    Add tests for bookmark test suite

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ based on https://github.com/udacity/frontend-nanodegree-feedreader
 favicon taken from http://fortawesome.github.io/
 
 * US 1: As a user, when I load a feed, I want to see a bookmark icon on every entry
-* US 2: As a user, when I click on the bookmark icon on a non-highlighted entry, I want the entry to be highlighted visually
-* US 3: As a user, when I click on the bookmark icon on a highlighted entyr, I want the entry to no longer be highlighted visually
-* US 4: As a user, when I load a feed, I don't want any entries to be bookmarked
+* US 2: As a user, when I load a feed, I don't want any entries to be bookmarked
+* US 3: As a user, when I click on the bookmark icon on a non-highlighted entry, I want the entry to be highlighted visually
+* US 4: As a user, when I click on the bookmark icon on a highlighted entyr, I want the entry to no longer be highlighted visually
--- jasmine/spec/feedreader-new-features.js
+++ jasmine/spec/feedreader-new-features.js
@@ -1,29 +1,57 @@
 /* feedreader-new-features.js
  *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
+ * This spec file contains all tests for unimplemented features. 
+ */
+
+/* We're placing all of our tests within the $() function,
+ * since some of these tests may require DOM elements. We want
+ * to ensure they don't run until the DOM is ready.
  */
 
 $(function() {
+
     /* Bookmark
      *
      * This test suites tests bookmarking and un-bookmarking of entries.
      * 
-     * User stories for this new feature can be found in the project README.md file
+     * User stories for this new feature can be found in the project README.md file.
      */
 
     describe('Bookmark', function () {
+
         /* All tests need a loaded feed */
-        beforeEach (function (done) {
+        beforeEach(function (done) {
             // remove any data from previous test
             $('.feed').empty();
 
             // load feed
-            loadFeed(0, function (done) {
+            loadFeed(0, function () {
+                entries = $('.feed').find('.entry').get();
                 done();
             });
         });
-    });
 
+        /* This tests US 1: every entry has a 'bookmark' icon */       
+        it('icon is visible for every entry.', function () {
+            entries.forEach(function (entry) {
+                expect($(entry).find('.bookmark-icon').length).toBe(1);
+            });
+        });
+
+        /* This tests US 2: on load, no entry is bookmarked */
+        it('state is off for all entries when feed is loaded.', function () {
+            entries.forEach(function (entry) {
+                expect($(entry).hasClass('bookmarked')).not.toBe(true);
+            });
+        });
+
+        /* This tests US 3 and 4: click on the 'bookmark' icon toggles bookmarked state */
+        it('icon toggles bookmark state on click', function () {
+            $(entries[0]).find('bookmark-icon').trigger('click');
+            expect($(entries[0]).hasClass('bookmarked')toBe('true'));
+            $(entries[0]).find('bookmark-icon').trigger('click');
+            expect($(entries[0]).hasClass('bookmarked')toBe('true'));
+        });
+    });
 
 }());
